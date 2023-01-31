134596706df0fc10fd483ee8c8c38fe4ac4541a4
Lucas <dexterfail@hotmail.com>
2023-01-31 14:05:52 -0300

    💬 Translate gridItem title

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Level } from "../../helpers/imc";
 import styles from "./GridItem.module.css";
 import upImage from "../../assets/up.png";
@@ -10,15 +11,31 @@ type Props = {
 };
 
 export const GridItem = ({ item }: Props) => {
+    const [title, setTitle] = React.useState("");
+
     const { language } = useLangs();
     const texts = langs;
 
+    React.useEffect(() => {
+        if(item.title === "Magreza") {
+            setTitle(texts.imc_levels[1][language])
+        }
+        if(item.title === "Normal") {
+            setTitle(texts.imc_levels[2])
+        }
+        if(item.title === "Sobrepeso") {
+            setTitle(texts.imc_levels[3][language])
+        }
+        if(item.title === "Obesidade") {
+            setTitle(texts.imc_levels[4][language])
+        }
+    },[language])
     return (
         <div className={styles.main} style={{ backgroundColor: item.color }}>
             <div className={styles.gridIcon}>
                 <img src={item.icon === "up" ? upImage : downImage} alt="" width="30" />
             </div>
-            <div className={styles.gridTitle}>{item.title}</div>
+            <div className={styles.gridTitle}>{title}</div>
 
             {item.yourImc &&
                 <div className={styles.yourImc}>{`${texts.imc_text.your_imc[1][language]} ${item.yourImc} ${texts.imc_text.your_imc[2]}`}</div>
